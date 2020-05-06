0bf2b4cc0b70a4f3f919c8f43d767af2eb44bd9a
thiagodff <thiago.fdornelles@gmail.com>
2020-05-06 14:42:24 -0300

    create blog page based on markdown arqs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,51 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+
+    return graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              html
+              frontmatter {
+                title
+                date(formatString: "DD/MM")
+                path
+              }
+            }
+          }
+        }
+      }      
+    `, { limit: 1000 }).then(result => {
+      if (result.errors) {
+        throw result.errors;
+      }
+
+      //create blog page
+      const posts = result.data.allMarkdownRemark.edges;
+
+      posts.forEach(({ node }) => {
+        const { html, frontmatter } = node;
+        
+        createPage({
+          path: frontmatter.path,
+          component: blogPostTemplate,
+          context: {
+          // Add optional context data to be inserted
+          // as props into the page component..
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          //
+          // The page "path" is always available as a GraphQL
+          // argument.
+            path: frontmatter.path,
+          }
+        })
+      });
+    })
+
+}
