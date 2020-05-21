f4e599eca4a365f46c16d49486126a1e4b7cc9e9
thiagodff <thiago.fdornelles@gmail.com>
2020-05-20 23:57:59 -0300

    add axios

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Blog do Thiago`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Thiago Fernandes Dornelles`,
+    description: `Blog com conteúdos sobre programação e carreira. Criado e editado por um programador full-stack apaixonado por tecnologia.`,
+    author: `@thiagodff`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,12 +28,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `home`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FCFCFC`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => {
@@ -10,10 +9,11 @@ const IndexPage = ({ data }) => {
   const { title, date } = frontmatter;
 
   return (
-    <Layout>
-      <SEO title="Home" />
-      <h1>{title}</h1>
+    <Layout title="blog">
+      <SEO title={title} />
+      <h1>{ title }</h1>
       <p>{ date }</p>
+      
       <div dangerouslySetInnerHTML={{ __html: html }} />
       
       <Link to="/">Go to home</Link>
@@ -26,7 +26,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { path: { eq: $path }}) {
       html
       frontmatter {
-        date
+        date(formatString: "DD/MM/YYYY, HH:MM")
         path
         title
       }
