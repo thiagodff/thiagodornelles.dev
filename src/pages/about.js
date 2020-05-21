7f1894cfaed0adfe707cef1b4565330b2de178d0
thiagodff <thiago.fdornelles@gmail.com>
2020-05-20 23:48:27 -0300

    add new test page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout title="about">
+    <SEO title="Thiago" />
+    <h1>Conheça um pouco sobre mim.</h1>
+  </Layout>
+)
+
+export default AboutPage
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = (props: PageProps) => (
-  <Layout>
+  <Layout title="404">
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2 ({props.path})</p>
